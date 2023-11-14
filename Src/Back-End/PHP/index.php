d5fc618c3e1b82db98d0b31d3776ce89a0dd2e63
Tagliassi <rafael.galafassi2005@gmail.com>
2023-11-14 19:01:12 -0300

    uptade

--- Src/Back-End/PHP/index.php
+++ Src/Back-End/PHP/index.php
@@ -1,6 +1,6 @@
 <?php
     // Define a URL para o redirecionamento
-    $url = "Location: /ClinicaA/medListar.php";
+    $url = "Location: /Pandoka/medListar.php";
 
     // Utiliza a função header para redirecionar para a URL especificada
     header($url);
