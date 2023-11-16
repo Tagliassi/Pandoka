04cab9dd1fce347da16d5f217843c87356c96765
vinicosper <cospervini@gmail.com>
2023-11-16 16:40:02 -0300

    commit de css paras telas

--- Src/Front-End/HTML/Pagina_Principal/pagina_cliente.php
+++ Src/Front-End/HTML/Pagina_Principal/pagina_cliente.php
@@ -3,24 +3,72 @@
 <head>
     <meta charset="UTF-8">
     <title>Sua Página</title>
-    <!-- Adicione aqui seus estilos CSS se necessário -->
+    <style>
+        /* Estilos CSS */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            background: url('../../../Front-End/IMGS/gif.gif') no-repeat center center fixed;
+            background-size: cover;
+            animation: animatedBackground 20s ease infinite;
+            height: 100vh;
+        }
+
+        nav {
+            background: linear-gradient(to right, #ff444b, #ff5c61);
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 10px 20px;
+        }
+
+        nav img {
+            width: 150px;
+            height: auto;
+        }
+
+        nav ul {
+            list-style: none;
+            display: flex;
+            gap: 20px;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+            color: #ffffff;
+            font-weight: bold;
+            padding: 10px 15px;
+            border-radius: 5px;
+            background-color: #000000;
+            transition: background-color 0.3s ease;
+        }
+
+        nav ul li a:hover {
+            background-color: #333333;
+        }
+        @keyframes animatedBackground {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+    </style>
 </head>
 <body>
 
-<!-- Barra de navegação -->
 <nav>
+    <img src="../../../Front-End/IMGS/logo_pandoka.jpeg" alt="Logo da Panificadora Pandoka">
     <ul>
-        <li><h2>Panificadora Pandoka</h2></li>
         <li><a href="../../../Back-End/PHP/catalogo_produtos.php">Catálogo de Produtos</a></li>
         <li><a href="../../../Back-End/PHP/carrinho_compras.php">Carrinho de Compras</a></li>
         <li><a href="../../HTML/Sign_And_Login/login.php">Sair</a></li>
     </ul>
 </nav>
-
-<!-- Conteúdo da página -->
-<div>
-    <!-- conteúdo aqui -->
-</div>
 
 </body>
 </html>
--- Src/Front-End/HTML/Sign_And_Login/login.php
+++ Src/Front-End/HTML/Sign_And_Login/login.php
@@ -1,57 +1,164 @@
-<?php
-session_start();
-require '../../../Back-End/PHP/conectaBD.php';
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Login - Panificadora Online</title>
+    <style>
+        /* Estilos CSS */
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            background: url('../../../Front-End/IMGS/gif.gif') center center no-repeat;
+            background-size: cover;
+            animation: animatedBackground 20s ease infinite;
+        }
 
-// Cria conexão
-$conn = mysqli_connect($servername, $username, $password, $database);
+        form {
+            width: 90%;
+            max-width: 600px;
+            height: 200px;
+            padding: 30px;
+            border-radius: 10px;
+            background-color: #;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            align-items: center;
+            justify-content: center;
+            
+        }
 
-// Verifica conexão
-if (!$conn) {
-    die("<strong> Falha de conexão: </strong>" . mysqli_connect_error());
-}
-// Configura para trabalhar com caracteres acentuados do português
-mysqli_query($conn,"SET NAMES 'utf8'");
-mysqli_query($conn,'SET character_set_connection=utf8');
-mysqli_query($conn,'SET character_set_client=utf8');
-mysqli_query($conn,'SET character_set_results=utf8');
+        label {
+            font-weight: bold;
+            color: #333333;
+        }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $login = $_POST['login'];
+        input[type="text"],
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            justify-content: center;
+            
+        }
 
-    // Verifica se é um cliente
-    $query_cliente = "SELECT * FROM Clientes WHERE cpf = '$login'";
-    $result_cliente = $conn->query($query_cliente);
+        input[type="submit"] {
+            background-color: #000000;
+            color: #ffffff;
+            font-weight: bold;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            font-size: 16px;
+            justify-content: center;
+            width: 200px;
+            margin-left: 200px;
+        }
 
-    if ($result_cliente->num_rows == 1) {
-        $cliente = $result_cliente->fetch_assoc();
-        $_SESSION['login'] = $login;
-        $_SESSION['id_cliente'] = $cliente['id_cliente'];
-        header("Location: ../../../Front-End/HTML/Pagina_Principal/pagina_cliente.php");
-        exit();
+        input[type="submit"]:hover {
+            background-color: #333333;
+        }
+
+        a {
+            display: block;
+            text-align: bottom;
+            margin-top: 20px;
+            color: #333333;
+            text-decoration: none;
+            width: 80px;
+            justify-content: center;
+            margin-left: 245px;
+        }
+
+        strong {
+            color: #ff6347;
+            font-weight: bold;
+        }
+
+        /* Estilos adicionais para o botão de cadastrar */
+        .btn-cadastrar {
+            display: flex;
+            text-align: bottom;
+            background-color: #000000;
+            color: #ffffff;
+            padding: 15px;
+            border-radius: 5px;
+            text-decoration: none;
+            transition: background-color 0.3s ease;
+            font-weight: bold;
+            font-size: 18px;
+            margin-top: 20px;
+            justify-content: center;
+        }
+
+        .btn-cadastrar:hover {
+            background-color: #333333;
+        }
+        @keyframes animatedBackground {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+    </style>
+</head>
+<body>
+    <?php
+    session_start();
+    require '../../../Back-End/PHP/conectaBD.php';
+
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) {
+        die("<strong> Falha de conexão: </strong>" . mysqli_connect_error());
     }
 
-    // Verifica se é um funcionário
-    $query_funcionario = "SELECT * FROM Funcionarios WHERE nome = '$login'";
-    $result_funcionario = $conn->query($query_funcionario);
+    mysqli_query($conn,"SET NAMES 'utf8'");
+    mysqli_query($conn,'SET character_set_connection=utf8');
+    mysqli_query($conn,'SET character_set_client=utf8');
+    mysqli_query($conn,'SET character_set_results=utf8');
 
-    if ($result_funcionario->num_rows == 1) {
-        $funcionario = $result_funcionario->fetch_assoc();
-        $_SESSION['login'] = $login;
-        $_SESSION['id_funcionario'] = $funcionario['id_funcionario'];
-        header("Location: ../../../Front-End/HTML/Pagina_Principal/pagina_funcionario.php");
-        exit();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $login = $_POST['login'];
+
+        $query_cliente = "SELECT * FROM Clientes WHERE cpf = '$login'";
+        $result_cliente = $conn->query($query_cliente);
+
+        if ($result_cliente->num_rows == 1) {
+            $cliente = $result_cliente->fetch_assoc();
+            $_SESSION['login'] = $login;
+            $_SESSION['id_cliente'] = $cliente['id_cliente'];
+            header("Location: ../../../Front-End/HTML/Pagina_Principal/pagina_cliente.php");
+            exit();
+        }
+
+        $query_funcionario = "SELECT * FROM Funcionarios WHERE nome = '$login'";
+        $result_funcionario = $conn->query($query_funcionario);
+
+        if ($result_funcionario->num_rows == 1) {
+            $funcionario = $result_funcionario->fetch_assoc();
+            $_SESSION['login'] = $login;
+            $_SESSION['id_funcionario'] = $funcionario['id_funcionario'];
+            header("Location: ../../../Front-End/HTML/Pagina_Principal/pagina_funcionario.php");
+            exit();
+        }
+
+        echo "Login inválido para clientes ou funcionários.";
     }
-
-    echo "Login inválido para clientes ou funcionários.";
-}
-?>
-
-<!-- Formulário de Login -->
-<form method="post" action="">
-    <label for="login">Login (CPF para clientes / Nome para funcionários):</label><br>
-    <input type="text" id="login" name="login"><br>
-    <input type="submit" value="Entrar">
-</form>
-
-<!-- Link para a página de cadastro -->
-<a href="./sign.php">Cadastrar</a>
+    ?>
+ <form method="post" action="">
+        <label for="login">Login (CPF para clientes / Nome para funcionários):</label><br>
+        <input type="text" id="login" name="login"><br>
+        <input type="submit" value="Entrar">
+        <a href="./sign.php" class="btn-cadastrar">Cadastrar</a>
+    </form>
+</body>
+</html>
